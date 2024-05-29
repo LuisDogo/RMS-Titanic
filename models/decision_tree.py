4bbb9d99dc34a53571ab633dd78e8c3547aac6ef
luisDogo <luisdogoms@gmail.com>
2024-05-28 20:38:42 -0600

    added confussion matrix and cleaning update

--- models/decision_tree.py
+++ models/decision_tree.py
@@ -1,14 +1,11 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from cleaning import test, train, t_size, rndom_seed, cols
-
-X_train, X_test, y_train, y_test = train_test_split(train[cols], train.Survived, test_size = t_size, random_state = rndom_seed)
+from cleaning import X_train, X_test, y_train, y_test
 
 # Create and train the model
 model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
+dtc = model.fit(X_train, y_train)
 
 # Make predictions
 y_pred = model.predict(X_test)
--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -2,12 +2,11 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from cleaning import test, train, t_size, rndom_seed, cols
+from cleaning import X_train, X_test, y_train, y_test
 
-X_train, X_test, y_train, y_test = train_test_split(train[cols], train.Survived, test_size = t_size, random_state = rndom_seed)
-
+# Create and train the model
 model = GaussianNB()
-model.fit(X_train, y_train)
+nb = model.fit(X_train, y_train)
 
 # Make predictions
 y_pred = model.predict(X_test)
